1ae6c9b761af346a2bfd97498cdac10f7bbec20e
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-05 13:08:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#mkdir .a && cd .a
+mkdir .a && cd .a
 #git clone --quiet https://github.com/artemisfowl004/tobrot && cd tobrot
 #python3 -m tobrot & cd ~
 git clone --quiet https://github.com/artemisfowl004/tobrot && cd tobrot
