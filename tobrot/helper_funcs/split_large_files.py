15360f77730b87e4c63a852975f82f9c3dd1a2de
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-08 12:27:05 +0530

    Update split_large_files.py

--- tobrot/helper_funcs/split_large_files.py
+++ tobrot/helper_funcs/split_large_files.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Akshay C / Shrimadhav U K / YK
+# (c) Shrimadhav U K
 
-import asyncio
-import logging
-import os
-import time
-
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
 # the logging things
-from tobrot import MAX_TG_SPLIT_FILE_SIZE, SP_LIT_ALGO_RITH_M
+import logging
+import math
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -18,6 +12,17 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
+
+import asyncio
+import os
+import time
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+from tobrot import (
+    MAX_TG_SPLIT_FILE_SIZE
+    SP_LIT_ALGO_RITH_M
+)
 
 
 async def split_large_files(input_file):
@@ -49,7 +54,6 @@ async def split_large_files(input_file):
         )
         # casting to int cuz float Time Stamp can cause errors
         minimum_duration = int(minimum_duration)
-
         LOGGER.info(minimum_duration)
         # END: proprietary
         start_time = 0
@@ -57,15 +61,13 @@ async def split_large_files(input_file):
         base_name = os.path.basename(input_file)
         input_extension = base_name.split(".")[-1]
         LOGGER.info(input_extension)
-
         i = 0
         flag = False
 
         while end_time <= total_duration:
             LOGGER.info(i)
             # file name generate
-            parted_file_name = "{}_PART_{}.{}".format(
-                str(base_name), str(i).zfill(5), str(input_extension))
+            parted_file_name = "{}_PART_{}.{}".format(str(base_name),str(i).zfill(5),str(input_extension))
 
             output_file = os.path.join(new_working_directory, parted_file_name)
             LOGGER.info(output_file)
@@ -79,18 +81,18 @@ async def split_large_files(input_file):
                 f"Start time {start_time}, End time {end_time}, Itr {i}"
             )
 
-            # adding offset of 3 seconds to ensure smooth playback
+            # adding offset of 3 seconds to ensure smooth playback 
             start_time = end_time - 3
             end_time = end_time + minimum_duration
             i = i + 1
-
             if (end_time > total_duration) and not flag:
-                end_time = total_duration
-                flag = True
+                 end_time = total_duration
+                 flag = True
             elif flag:
                 break
 
     elif SP_LIT_ALGO_RITH_M.lower() == "hjs":
+
         # handle normal files here
         o_d_t = os.path.join(
             new_working_directory,
@@ -142,6 +144,8 @@ async def cult_small_video(video_file, out_put_file_name, start_time, end_time):
         "-2",
         "-c",
         "copy",
+        "-map",
+        "0",
         out_put_file_name
     ]
     process = await asyncio.create_subprocess_exec(
@@ -157,7 +161,6 @@ async def cult_small_video(video_file, out_put_file_name, start_time, end_time):
     LOGGER.info(t_response)
     return out_put_file_name
 
-
 async def run_comman_d(command_list):
     process = await asyncio.create_subprocess_exec(
         *command_list,
@@ -170,3 +173,67 @@ async def run_comman_d(command_list):
     e_response = stderr.decode().strip()
     t_response = stdout.decode().strip()
     return t_response, e_response
+
+async def split_file_to_parts_or_by_start_end_seconds(message, input_file, no_of_parts, start_seconds, end_seconds):
+    working_directory = os.path.dirname(os.path.abspath(input_file))
+    new_working_directory = os.path.join(
+        working_directory,
+        str(time.time())
+    )
+    # create download directory, if not exist
+    if not os.path.isdir(new_working_directory):
+        os.makedirs(new_working_directory)
+    if input_file.upper().endswith(("MKV", "MP4", "WEBM", "MP3", "M4A", "FLAC", "WAV")):
+        # handle video / audio files here
+        metadata = extractMetadata(createParser(input_file))
+        total_duration = 0
+        if metadata.has("duration"):
+            total_duration = metadata.get('duration').seconds
+        # proprietary logic to get the seconds to trim (at)
+        LOGGER.info(total_duration)
+        total_file_size = os.path.getsize(input_file)
+        LOGGER.info(total_file_size)
+        base_name = os.path.basename(input_file)
+        input_extension = base_name.split(".")[-1]
+        file_name_without_extension= base_name.replace(f".{input_extension}","")
+        if no_of_parts is None:
+            if total_duration >= end_seconds:
+                output_file = os.path.join(new_working_directory, f'{file_name_without_extension}_SAMPLE.{input_extension}')
+                LOGGER.info(await cult_small_video(
+                    input_file,
+                    output_file,
+                    str(start_seconds),
+                    str(end_seconds)
+                ))
+            else:
+                await message.reply_text("given end timestamp is out of range of actual video runtime")
+                return None
+        else:
+            duration_per_part = math.floor(total_duration / no_of_parts)
+            LOGGER.info(duration_per_part)
+            # END: proprietary
+            start_time = 0
+            end_time = duration_per_part
+
+            LOGGER.info(input_extension)
+            i = 0
+            while end_time < total_duration+2:
+                LOGGER.info(f'part - {i},starttime={start_time},endtime={end_time}')
+                parted_file_name = ""
+                parted_file_name += str(file_name_without_extension)
+                parted_file_name += "_PART_"
+                parted_file_name += str(i).zfill(3)
+                parted_file_name += "."
+                parted_file_name += str(input_extension)
+                output_file = os.path.join(new_working_directory, parted_file_name)
+                LOGGER.info(output_file)
+                LOGGER.info(await cult_small_video(
+                    input_file,
+                    output_file,
+                    str(start_time),
+                    str(end_time)
+                ))
+                start_time = end_time
+                end_time = end_time + duration_per_part
+                i = i + 1
+    return new_working_directory
