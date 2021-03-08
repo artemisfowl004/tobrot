de9894b07944838d3c95b788df1d594ba6e9f483
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-08 11:43:31 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir .a && cd .a
-#git clone --quiet https://github.com/artemisfowl004/tobrot && cd tobrot
-#python3 -m tobrot & cd ~
-git clone --quiet https://github.com/artemisfowl004/tobrot && cd tobrot
-python3 -m tobrot;cd ~
