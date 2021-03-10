fd0ab40ec497fece2df4daecdd87c49f05a2f5d7
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-10 12:36:16 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -203,13 +203,13 @@ if __name__ == "__main__" :
     #
     upload_as_doc_handler = MessageHandler(
         upload_as_doc,
-        filters=Filters.command(os.environ.get("TOGGLE_DOC")) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(os.environ.get("TOGGLE_DOC", "toggledoc")) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_as_doc_handler)
     #
     upload_as_video_handler = MessageHandler(
         upload_as_video,
-        filters=Filters.command(os.environ.get("TOGGLE_VIDEO")) & Filters.chat(chats=AUTH_CHANNEL)
+        filters=Filters.command(os.environ.get("TOGGLE_VIDEO", "togglevideo")) & Filters.chat(chats=AUTH_CHANNEL)
     )
     app.add_handler(upload_as_video_handler)
 
