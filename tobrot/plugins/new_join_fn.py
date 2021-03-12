e8fa34da42fedf4f8459fcd9ff42377af7b57a02
artemisfowl004 <69702795+artemisfowl004@users.noreply.github.com>
2021-03-12 12:08:40 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,5 +38,5 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""""", disable_web_page_preview=True)
+    await message.reply_text("""https://telegra.ph/Help-Message-03-12""", disable_web_page_preview=True)
 
